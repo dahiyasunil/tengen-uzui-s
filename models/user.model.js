52be41f1c6d1528e19c6144eff86e34c13d717d3
dahiyasunil <sunil.dahiya13@gmail.com>
2025-02-04 07:11:08 +0530

    chore: updated model to have model referencing for wishlist, bag and orders

--- models/user.model.js
+++ models/user.model.js
@@ -1,22 +1,22 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    mobileNumber: {
-        type: Number,
-        required: true,
-        unique: true
-    },
-    name: {
-        type: String,
-    },
-    emailId: {
-        type: String,
-    },
-    addresses: { type: Array, "default": [] },
-    wishlist: [{ type: String }],
-    bag: [{ type: String }],
-    orders: [{ type: String }]
-})
+  mobileNumber: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+  },
+  emailId: {
+    type: String,
+  },
+  addresses: { type: Array, default: [] },
+  wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+  bag: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+  orders: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+});
 
 const User = mongoose.model("User", userSchema);
-module.exports =User;
+module.exports = User;
