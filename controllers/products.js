98f2dfd3312a1680f832be36b8d761ec44d281a5
dahiyasunil <sunil.dahiya13@gmail.com>
2025-02-04 07:13:33 +0530

    chore: updated logic to return only searched product when search param is available or else return all products

--- controllers/products.js
+++ controllers/products.js
@@ -3,13 +3,15 @@ const Product = require("../models/product.model");
 const handleGetAllProducts = async (req, res) => {
   try {
     const keywords = req.query.search;
-    const allProducts = keywords? await Product.find({ $text: { $search: keywords }}): await Product.find();
+    const allProducts = keywords
+      ? await Product.find({ $text: { $search: keywords } })
+      : await Product.find();
     return res.status(200).json(allProducts);
   } catch (err) {
     console.error(
-      "An error occured while trying to fetch all products.\nError: \n" + err
+      "An error occured while trying to fetch all products.\nError: \n" + err,
     );
-    return res.status(500).json({ err: "Please try again later!" });
+    next(err);
   }
 };
 
