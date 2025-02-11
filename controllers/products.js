6850e6ce6273172a0c9a29abad1c769307736d66
dahiyasunil <sunil.dahiya13@gmail.com>
2025-02-11 07:11:07 +0530

    feat: added logic to enrich products with isWishlisted Flag

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,44 @@
 const Product = require("../models/product.model");
+const User = require("../models/user.model");
 
-const handleGetAllProducts = async (req, res) => {
+const getProducts = async (keywords) => {
+  return keywords
+    ? await Product.find({ $text: { $search: keywords } })
+    : await Product.find();
+};
+
+const enrichWithWishlistFlag = (products, wishlistItems) => {
+  const enrichedProducts = products.map((product) => {
+    const wishlistedItem = wishlistItems.find(
+      (item) => item === product._id.toString(),
+    );
+    let enrichedProduct = JSON.parse(JSON.stringify(product));
+    if (wishlistedItem) {
+      enrichedProduct.isWishlisted = true;
+    } else {
+      enrichedProduct.isWishlisted = false;
+    }
+    return enrichedProduct;
+  });
+  return enrichedProducts;
+};
+
+const handleGetAllProducts = async (req, res, next) => {
   try {
     const keywords = req.query.search;
-    const allProducts = keywords
-      ? await Product.find({ $text: { $search: keywords } })
-      : await Product.find();
-    return res.status(200).json(allProducts);
+    const mobileNo = req.get("X-Mobile-No");
+    let products = [];
+    if (mobileNo) {
+      const user = await User.findOne({ mobileNumber: mobileNo });
+      const wishlistItems = user.wishlist.map((item) => item.toString());
+      products = enrichWithWishlistFlag(
+        await getProducts(keywords),
+        wishlistItems,
+      );
+    } else {
+      products = await getProducts(keywords);
+    }
+    return res.status(200).json(products);
   } catch (err) {
     console.error(
       "An error occured while trying to fetch all products.\nError: \n" + err,
--- controllers/user.js
+++ controllers/user.js
@@ -26,7 +26,7 @@ const handleAddToWishlist = async (req, res, next) => {
       { new: true },
     );
     console.log(user);
-    res.status(200).json({ "wishlist":user.wishlist });
+    res.status(200).json({ wishlist: user.wishlist });
   } catch (err) {
     console.error(
       `An error occured while trying to add product to wishlist.\nError:\n${err}`,
@@ -44,8 +44,8 @@ const handleRemoveFromWishlist = async (req, res, next) => {
       { new: true },
     );
     console.log(user);
-    
-    res.status(200).json({ "wishlist":user.wishlist });
+
+    res.status(200).json({ wishlist: user.wishlist });
   } catch (err) {
     console.error(
       `An error occured while trying to remove product to wishlist.\nError:\n${err}`,
@@ -58,7 +58,14 @@ const handleGetWishlistItems = async (req, res, next) => {
   try {
     const userId = req.params.userId;
     const user = await User.findOne({ _id: userId }).populate("wishlist");
-    return res.status(200).json({ wishlist: user.wishlist });
+    const wishlistedItems = user.wishlist.map((item) => {
+      let enrichedProduct = JSON.parse(JSON.stringify(item));
+      enrichedProduct.isWishlisted = true;
+      return enrichedProduct;
+    });
+    console.log(wishlistedItems);
+
+    return res.status(200).json({ wishlist: wishlistedItems });
   } catch (err) {
     console.error(
       `An error occured while trying to fetch wishlist.\nError:\n${err}`,
