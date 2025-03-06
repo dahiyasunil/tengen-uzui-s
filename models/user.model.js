e37bd931e518b862b61082ffc13a60cf268a66a4
dahiyasunil <sunil.dahiya13@gmail.com>
2025-03-06 23:28:51 +0530

    chore: added property on order to store creation time

--- models/user.model.js
+++ models/user.model.js
@@ -36,6 +36,7 @@ const userSchema = new mongoose.Schema({
       deliveryAddress: { type: Object, required: true },
       totalDiscount: { type: Number, required: true },
       totalPrice: { type: Number, required: true },
+      createdAt: { type: Date, default: Date.now },
     },
   ],
 });
